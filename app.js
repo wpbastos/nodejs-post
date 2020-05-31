76b7266dd9e35331a61080355fb6b449e918fcdb
Wellington P Bastos <wpbastos@live.com>
2020-05-31 11:09:01 -0400

    Added authentication

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -12,13 +13,13 @@ const optionsDB = {
   useUnifiedTopology: true,
   useCreateIndex: true,
   retryWrites: true,
-  authSource: 'admin',
+  // authSource: 'admin',
 };
-const urlDB = 'mongodb://admin:admin@localhost:27017/post';
+const urlDB = 'mongodb+srv://postUser:JpNhXHDrLsH9mnNP@blog-rxmwl.azure.mongodb.net/blog';
 mongoose
   .connect(urlDB, optionsDB)
   .then(() => {
-    console.log('Connected to database!');
+    console.info('Connected to database!');
   })
   .catch((error) => {
     console.error('Connection failed!');
@@ -31,11 +32,12 @@ app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   next();
 });
 
 app.use('/api/posts', postRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,7 @@ const multer = require('multer');
 const express = require('express');
 
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -27,42 +28,6 @@ const storage = multer.diskStorage({
   },
 });
 
-router.post('', multer({ storage: storage }).single('image'), (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host');
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + '/images/' + req.file.filename,
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-      },
-    });
-  });
-});
-
-router.put('/:id', multer({ storage: storage }).single('image'), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if (req.file) {
-    const url = req.protocol + '://' + req.get('host');
-    imagePath = url + '/images/' + req.file.filename;
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-  });
-  console.log(post);
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    res.status(200).json({ message: 'Update successful!' });
-  });
-});
-
 router.get('', (req, res, next) => {
   const pageSize = +req.query.pagesize;
   const currentPage = +req.query.page;
@@ -74,7 +39,7 @@ router.get('', (req, res, next) => {
   postQuery
     .then((documents) => {
       fetchedPosts = documents;
-      return Post.count();
+      return Post.countDocuments();
     })
     .then((count) => {
       res.status(200).json({
@@ -95,9 +60,43 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename,
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      post: {
+        ...createdPost,
+        id: createdPost._id,
+      },
+    });
+  });
+});
+
+router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + '://' + req.get('host');
+    imagePath = url + '/images/' + req.file.filename;
+  }
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: imagePath,
+  });
+  Post.updateOne({ _id: req.params.id }, post).then((result) => {
+    res.status(200).json({ message: 'Update successful!' });
+  });
+});
+
+router.delete('/:id', checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
     res.status(200).json({ message: 'Post deleted!' });
   });
 });
